d847270eade1359ac9e03a9fed2b4041731e7e01
rocketman <20303031+rocketman-21@users.noreply.github.com>
2024-12-13 15:24:05 -0300

    fix backend

--- src/database/flows-schema.ts
+++ src/database/flows-schema.ts
@@ -50,52 +50,62 @@ export const stories = webSchema.table('Stories', {
 export const grant = pgTable('Grant', {
   id: text('id').primaryKey(),
   recipient: text('recipient').notNull(),
-  flowId: text('flowId').notNull(),
+  flow_id: text('flow_id').notNull(),
   submitter: text('submitter').notNull(),
-  parentContract: text('parentContract').notNull(),
-  isTopLevel: integer('isTopLevel').notNull(),
-  isFlow: integer('isFlow').notNull(),
+  parent_contract: text('parent_contract').notNull(),
+  is_top_level: integer('is_top_level').notNull(),
+  is_flow: integer('is_flow').notNull(),
   title: text('title').notNull(),
   description: text('description').notNull(),
   image: text('image').notNull(),
   tagline: text('tagline'),
   url: text('url'),
-  isRemoved: integer('isRemoved').notNull(),
-  isActive: integer('isActive').notNull(),
-  votesCount: text('votesCount').notNull(),
-  monthlyIncomingFlowRate: text('monthlyIncomingFlowRate').notNull(),
-  monthlyIncomingBaselineFlowRate: text(
-    'monthlyIncomingBaselineFlowRate'
+  is_removed: integer('is_removed').notNull(),
+  is_active: integer('is_active').notNull(),
+  votes_count: text('votes_count').notNull(),
+  monthly_incoming_flow_rate: text('monthly_incoming_flow_rate').notNull(),
+  monthly_incoming_baseline_flow_rate: text(
+    'monthly_incoming_baseline_flow_rate'
   ).notNull(),
-  monthlyIncomingBonusFlowRate: text('monthlyIncomingBonusFlowRate').notNull(),
-  monthlyOutgoingFlowRate: text('monthlyOutgoingFlowRate').notNull(),
-  monthlyRewardPoolFlowRate: text('monthlyRewardPoolFlowRate').notNull(),
-  monthlyBaselinePoolFlowRate: text('monthlyBaselinePoolFlowRate').notNull(),
-  monthlyBonusPoolFlowRate: text('monthlyBonusPoolFlowRate').notNull(),
-  bonusMemberUnits: text('bonusMemberUnits').notNull(),
-  baselineMemberUnits: text('baselineMemberUnits').notNull(),
-  totalEarned: text('totalEarned').notNull(),
-  activeRecipientCount: integer('activeRecipientCount').notNull(),
-  awaitingRecipientCount: integer('awaitingRecipientCount').notNull(),
-  challengedRecipientCount: integer('challengedRecipientCount').notNull(),
+  monthly_incoming_bonus_flow_rate: text(
+    'monthly_incoming_bonus_flow_rate'
+  ).notNull(),
+  monthly_outgoing_flow_rate: text('monthly_outgoing_flow_rate').notNull(),
+  monthly_reward_pool_flow_rate: text(
+    'monthly_reward_pool_flow_rate'
+  ).notNull(),
+  monthly_baseline_pool_flow_rate: text(
+    'monthly_baseline_pool_flow_rate'
+  ).notNull(),
+  monthly_bonus_pool_flow_rate: text('monthly_bonus_pool_flow_rate').notNull(),
+  bonus_member_units: text('bonus_member_units').notNull(),
+  baseline_member_units: text('baseline_member_units').notNull(),
+  total_earned: text('total_earned').notNull(),
+  active_recipient_count: integer('active_recipient_count').notNull(),
+  awaiting_recipient_count: integer('awaiting_recipient_count').notNull(),
+  challenged_recipient_count: integer('challenged_recipient_count').notNull(),
   tcr: text('tcr').unique().notNull(),
   erc20: text('erc20').unique().notNull(),
   arbitrator: text('arbitrator').unique().notNull(),
-  tokenEmitter: text('tokenEmitter').unique().notNull(),
+  token_emitter: text('token_emitter').unique().notNull(),
   status: integer('status').notNull(),
-  challengePeriodEndsAt: integer('challengePeriodEndsAt').notNull(),
-  isDisputed: integer('isDisputed').notNull(),
-  isResolved: integer('isResolved').notNull(),
-  evidenceGroupID: text('evidenceGroupID').unique().notNull(),
-  createdAt: integer('createdAt').notNull(),
-  updatedAt: integer('updatedAt').notNull(),
-  baselinePool: text('baselinePool').notNull(),
-  bonusPool: text('bonusPool').notNull(),
-  managerRewardPool: text('managerRewardPool').notNull(),
-  superToken: text('superToken').notNull(),
-  managerRewardSuperfluidPool: text('managerRewardSuperfluidPool').notNull(),
-  managerRewardPoolFlowRatePercent: integer(
-    'managerRewardPoolFlowRatePercent'
+  challenge_period_ends_at: integer('challenge_period_ends_at').notNull(),
+  is_disputed: integer('is_disputed').notNull(),
+  is_resolved: integer('is_resolved').notNull(),
+  evidence_group_id: text('evidence_group_id').unique().notNull(),
+  created_at: integer('created_at').notNull(),
+  updated_at: integer('updated_at').notNull(),
+  baseline_pool: text('baseline_pool').notNull(),
+  bonus_pool: text('bonus_pool').notNull(),
+  manager_reward_pool: text('manager_reward_pool').notNull(),
+  super_token: text('super_token').notNull(),
+  manager_reward_superfluid_pool: text(
+    'manager_reward_superfluid_pool'
   ).notNull(),
-  baselinePoolFlowRatePercent: integer('baselinePoolFlowRatePercent').notNull(),
+  manager_reward_pool_flow_rate_percent: integer(
+    'manager_reward_pool_flow_rate_percent'
+  ).notNull(),
+  baseline_pool_flow_rate_percent: integer(
+    'baseline_pool_flow_rate_percent'
+  ).notNull(),
 });
